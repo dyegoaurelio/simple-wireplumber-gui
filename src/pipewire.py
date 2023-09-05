5f448027115dc5bf23b4a914507ef6c0f439a5f6
Dyego <dyegoaurelio@gmail.com>
2023-09-05 19:17:49 -0300

    organizing functions

--- src/pipewire.py
+++ src/pipewire.py
@@ -1,5 +1,10 @@
+import os
+import re
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
+
+PATTERN_IDS = r"id (\d+), type ([\w:\/]+)"
+PATTERN_DATA = r'(\w+\.\w+)\s*=\s*"([^"]+)"'
 
 
 @dataclass
@@ -33,42 +38,39 @@ disabled_input_devices = filter(lambda d: d.hidden, input_devices)
 active_output_devices = filter(lambda d: not d.hidden, output_devices)
 disabled_output_devices = filter(lambda d: d.hidden, output_devices)
 
-import os
 
-stream = os.popen("pw-cli list-objects")
-output = stream.read()
-import re
+def parse_pw_cli_ls_data(data: str) -> Dict[str, Dict[str, str]]:
+    parsed_data = {}
 
-data = output
+    last_match_character = -1
+    last_match_id = None
 
-pattern_ids = r"id (\d+), type ([\w:\/]+)"
-pattern_data = r'(\w+\.\w+)\s*=\s*"([^"]+)"'
+    def append_last_id_data(current_start: int):
+        if last_match_character < 0:
+            return
 
-parsed_data = {}
+        current_data = data[last_match_character:current_start]
+        matches = re.findall(PATTERN_DATA, current_data)
 
-last_match_character = -1
-current_start_character = -1
-last_match_id = None
+        parsed_data[last_match_id].update(dict(matches))
+
+    for match in re.finditer(PATTERN_IDS, data):
+        id_, type_ = match.groups()
+        parsed_data[id_] = {"type": type_}
+
+        append_last_id_data(match.start())
+
+        last_match_character = match.end()
+        last_match_id = id_
+
+    append_last_id_data(len(data))
+
+    return parsed_data
 
 
-def append_last_id_data(current_start: int):
-    if last_match_character < 0:
-        return
-
-    current_data = data[last_match_character:current_start]
-    matches = re.findall(pattern_data, current_data)
-
-    parsed_data[last_match_id].update(dict(matches))
+def get_pipewire_devices_data():
+    stream = os.popen("pw-cli list-objects")
+    return parse_pw_cli_ls_data(stream.read())
 
 
-for match in re.finditer(pattern_ids, data):
-    id_, type_ = match.groups()
-    parsed_data[id_] = {"type": type_}
-    key_value_pairs = []
-
-    append_last_id_data(match.start())
-
-    last_match_character = match.end()
-    last_match_id = id_
-
-append_last_id_data(len(data))
+pipewire_devices = get_pipewire_devices_data()
