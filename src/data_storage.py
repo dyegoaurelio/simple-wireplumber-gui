b972fd477e2c0391f1f9eb6df96f36185e8e3adb
Dyego <dyegoaurelio@gmail.com>
2023-09-06 17:15:11 -0300

    syncing app's config with wireplumber

--- src/data_storage.py
+++ src/data_storage.py
@@ -5,6 +5,52 @@ current_user = os.popen("echo $USER").read().strip()
 
 # Define the path where the configuration data will be stored
 CONFIG_PATH = f"/home/{current_user}/.config/simple-wireplumber-gui/config.json"
+WIREPLUMBER_CONFIG_FOLDER = f"/home/{current_user}/.config/wireplumber"
+WIREPLUMBER_DEVICE_DESCRIPTION_CONFIG_PATH = (
+    f"{WIREPLUMBER_CONFIG_FOLDER}/main.lua.d/52-SWG-rename-devices.lua"
+)
+
+change_device_template = """
+rule = {{
+  matches = {{
+    {{
+      {{ "device.name", "equals", "{device_name}" }},
+    }},
+  }},
+  apply_properties = {{
+{properties}
+  }},
+}}
+
+table.insert(alsa_monitor.rules,rule)
+
+"""
+
+property_template = '      ["{key}"] = "{value}",\n'
+
+
+def _apply_new_device_description(data: dict | None):
+    if data is None:
+        return
+
+    os.makedirs(
+        os.path.dirname(WIREPLUMBER_DEVICE_DESCRIPTION_CONFIG_PATH), exist_ok=True
+    )
+
+    script_text = ""
+
+    for name, properties_data in data.items():
+        properties = ""
+
+        for key, value in properties_data.items():
+            properties += property_template.format(key=key, value=value)
+
+        script_text += change_device_template.format(
+            device_name=name, properties=properties
+        )
+
+    with open(WIREPLUMBER_DEVICE_DESCRIPTION_CONFIG_PATH, "w") as f:
+        f.write(script_text)
 
 
 def _save_config(data):
@@ -15,6 +61,7 @@ def _save_config(data):
         data (dict): The dictionary to be saved as a configuration.
     """
     os.makedirs(os.path.dirname(CONFIG_PATH), exist_ok=True)
+    _apply_new_device_description(data.get("devices_new_description", None))
     with open(CONFIG_PATH, "w") as config_file:
         json.dump(data, config_file)
 
@@ -39,7 +86,10 @@ def add_device_device_new_description(device_name: str, new_description: str):
     current_config = load_config()
     current_new_descriptions = current_config.get("devices_new_description", {})
 
-    current_new_descriptions[device_name] = {"device.description": new_description}
+    current_new_descriptions[device_name] = {
+        "device.description": new_description,
+        "device.nick": new_description,
+    }
 
     current_config["devices_new_description"] = current_new_descriptions
 
