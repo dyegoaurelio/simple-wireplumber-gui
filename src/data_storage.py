fbb0d52561b5a088aec922323b4d4f04bb845eeb
Dyego <dyegoaurelio@gmail.com>
2023-09-06 11:17:43 -0300

    fixing home folder dir

--- src/data_storage.py
+++ src/data_storage.py
@@ -1,13 +1,16 @@
 import json
 import os
 
+current_user = os.popen("echo $USER").read().strip()
+
 # Define the path where the configuration data will be stored
-CONFIG_PATH = "~/.config/simple-wireplumber-gui/config.json"
+CONFIG_PATH = f"/home/{current_user}/config/simple-wireplumber-gui/config.json"
+
 
 def _save_config(data):
     """
     Save a dictionary as a JSON configuration file.
-    
+
     Args:
         data (dict): The dictionary to be saved as a configuration.
     """
@@ -15,10 +18,11 @@ def _save_config(data):
     with open(CONFIG_PATH, "w") as config_file:
         json.dump(data, config_file)
 
+
 def load_config():
     """
     Load the configuration data from the JSON file.
-    
+
     Returns:
         dict: The loaded configuration data as a dictionary.
     """
@@ -30,12 +34,13 @@ def load_config():
         print(f"Config file '{CONFIG_PATH}' not found. Returning an empty dictionary.")
         return {}
 
+
 def add_device_device_new_description(device_name: str, new_description: str):
     current_config = load_config()
-    current_new_descriptions = current_config.get('devices_new_description', {})
-    
-    current_new_descriptions[device_name] = { "device.description" : new_description}
-    
-    current_config['devices_new_description'] = current_new_descriptions
-    
+    current_new_descriptions = current_config.get("devices_new_description", {})
+
+    current_new_descriptions[device_name] = {"device.description": new_description}
+
+    current_config["devices_new_description"] = current_new_descriptions
+
     _save_config(current_config)
