38668156c7d401adea7ad14f5432f9eaf84c0aa8
Dyego <dyegoaurelio@gmail.com>
2023-09-05 22:49:07 -0300

    starting to show real devices on ui

--- src/pipewire.py
+++ src/pipewire.py
@@ -12,31 +12,13 @@ class Device:
     id: str
     hidden: bool
     name: str
+    description: str
+    nick: str
 
 
-input_devices: List[Device] = [
-    Device("dev-1", False, "Device 1"),
-    Device("dev-2", False, "Device 2"),
-    Device("dev-3", True, "Device 3"),
-    Device("dev-4", False, "Device 4"),
-    Device("dev-5", True, "Device 5"),
-]
+input_devices: List[Device] = []
 
-output_devices: List[Device] = [
-    Device("dev-6", False, "Device 6"),
-    Device("dev-7", False, "Device 7"),
-    Device("dev-8", True, "Device 8"),
-    Device("dev-9", False, "Device 9"),
-    Device("dev-10", True, "Device 10"),
-    Device("dev-11", True, "Device 11"),
-    Device("dev-12", False, "Device 12"),
-]
-
-active_input_devices = filter(lambda d: not d.hidden, input_devices)
-disabled_input_devices = filter(lambda d: d.hidden, input_devices)
-
-active_output_devices = filter(lambda d: not d.hidden, output_devices)
-disabled_output_devices = filter(lambda d: d.hidden, output_devices)
+output_devices: List[Device] = []
 
 
 def parse_pw_cli_ls_data(data: str) -> Dict[str, Dict[str, str]]:
@@ -68,9 +50,37 @@ def parse_pw_cli_ls_data(data: str) -> Dict[str, Dict[str, str]]:
     return parsed_data
 
 
-def get_pipewire_devices_data():
+def get_pipewire_objects_data():
     stream = os.popen("pw-cli list-objects")
     return parse_pw_cli_ls_data(stream.read())
 
 
-pipewire_devices = get_pipewire_devices_data()
+def get_pipewire_devices_data():
+    pipewire_objects = get_pipewire_objects_data()
+
+    pipewire_devices = filter(
+        lambda obj: obj.get("media.class") == "Audio/Device",
+        pipewire_objects.values(),
+    )
+
+    return pipewire_devices
+
+
+output_devices = list(
+    map(
+        lambda d: Device(
+            id=d["object.serial"],
+            name=d["device.name"],
+            description=d["device.description"],
+            nick=d["device.name"],
+            hidden=False,
+        ),
+        get_pipewire_devices_data(),
+    )
+)
+
+active_input_devices = filter(lambda d: not d.hidden, input_devices)
+disabled_input_devices = filter(lambda d: d.hidden, input_devices)
+
+active_output_devices = filter(lambda d: not d.hidden, output_devices)
+disabled_output_devices = filter(lambda d: d.hidden, output_devices)
